cd08ad3c36147486f2cc3f1d7ffbfe293ce3e968
Daniel Kříž <Daniel.kriz@protonmail.com>
2021-12-01 23:23:02 +0100

    day1: remove redundant mutable var

--- day1/part1.rs
+++ day1/part1.rs
@@ -2,11 +2,10 @@ use std::io;
 use std::io::prelude::*;
 
 fn main() {
-    let mut prev: i32 = 99999;
-    let mut total_increased: i32 = 0;
-    let mut curr: i32;
+    let mut prev: i32 = 0;
+    let mut total_increased: i32 = -1;
     for line in io::stdin().lock().lines() {
-        curr = line.unwrap().parse::<i32>().unwrap();
+        let curr: i32 = line.unwrap().parse::<i32>().unwrap();
         if curr > prev {
             total_increased += 1;
         }
