93a5a6cc14546aee4570270f3ed141c83946d927
Daniel Kříž <Daniel.kriz@protonmail.com>
2021-12-02 00:33:49 +0100

    day1: add better version of part2

--- day1/part2.rs
+++ day1/part2.rs
@@ -1,49 +1,24 @@
 use std::io;
 use std::io::prelude::*;
-use std::convert::TryInto;
 
 fn main() {
-    let mut total_increased: i32 = -1;
-    let mut i: u32 = 0;
-    let mut windows: [i32;4] = [-1, -1, -1, -1];
-    let mut prev: i32 = -1;
-    for line in io::stdin().lock().lines() {
-        i += 1;
-        let input = line.unwrap().parse::<i32>().unwrap();
-        if i % 4 == 1 {
-            windows[0] += input;
-            windows[2] += input;
-            windows[3] += input;
-            if windows[2] > prev {
-                total_increased += 1;
-            }
-        } else if i % 4 == 2 {
-            windows[0] += input;
-            windows[1] += input;
-            windows[3] += input;
-            if windows[3] > prev {
-                total_increased += 1;
-            }
-        } else if i % 4 == 3 {
-            windows[0] += input;
-            windows[1] += input;
-            windows[2] += input;
-            if windows[0] > prev {
-                total_increased += 1;
-            }
-        } else {
-            windows[1] += input;
-            windows[2] += input;
-            windows[3] += input;
-            if windows[1] > prev {
-                total_increased += 1;
-            }
-        }
-        let idx: usize = ((i + 1) % 4).try_into().unwrap();
-        prev = windows[idx];
-        windows[idx] = 0;
-    }
     // there is an error introduced in this approach that we have to subtract
-    total_increased -= 2;
+    let mut total_increased: i32 = -3;
+    let mut scan: [i32; 3] = [0, 0, 0];
+    let mut prev: i32 = -1;
+    for (i, line) in io::stdin().lock().lines().enumerate() {
+        let input = line.unwrap().parse::<i32>().unwrap();
+        match i % 3 {
+            0 => scan[0] = input,
+            1 => scan[1] = input,
+            2 => scan[2] = input,
+            _ => (),
+        }
+        let arr_sum: i32 = scan.iter().sum::<i32>();
+        if arr_sum > prev {
+            total_increased += 1;
+        }
+        prev = arr_sum;
+    }
     println!("{}", total_increased.to_string());
 }
