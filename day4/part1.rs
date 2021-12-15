88d2583e78c8d1e05c2c10da892f38bdc3224546
Daniel Kříž <Daniel.kriz@protonmail.com>
2021-12-15 17:36:46 +0100

    wip: day4: more of part1

--- day4/part1.rs
+++ day4/part1.rs
@@ -1,13 +1,13 @@
 use std::io;
 use std::io::prelude::*;
 
-/// TODO: some function that will check if number are done
+// TODO: some function that will check if number are done
 
 fn main() {
     let stdin = io::stdin();
     let mut lines_iter = stdin.lock().lines();
-    ///
-    /// after this, the first line is removed from the iterator
+
+    // after this, the first line is removed from the iterator
     let first_line = lines_iter.next().unwrap().unwrap();
     let drafts: Vec<i32> = first_line
         .split(",")
@@ -17,19 +17,76 @@ fn main() {
         .collect();
 
     let mut i = 0;
-    let mut matrices: Vec<[[i32; 5]; 5]>;
+    // there is one more row and column for checking if number was chosen
+    let mut matrices: Vec<[[i32; 6]; 6]> = Vec::new();
+    let mut new_matrix: [[i32; 6]; 6] = [[0; 6]; 6];
     for unwrapped_line in lines_iter {
         let line = unwrapped_line.unwrap();
         if line.is_empty() {
             continue
         }
 
-        if i == 5 {
-            i = 0;
+        for (j, num) in line
+            .split(' ')
+            .map(|x| x.trim())
+            .filter(|x| !x.is_empty())
+            .map(|x| x.parse::<i32>().unwrap())
+            .enumerate() {
+            new_matrix[(i%5) as usize][j] = num;
+        }
+
+        if i != 0 && i % 5 == 0 {
+            matrices.push(new_matrix);
+            new_matrix = [[0; 6]; 6];
         }
 
         i += 1;
-        /// fill the matrices vector
-        println!("{}", line);
+    }
+    for matrix in matrices.iter() {
+        for line in matrix {
+            for num in line {
+                print!("{} ", num);
+            }
+            println!("");
+        }
+        println!("");
+    }
+    const CHECK: usize = 5;
+    for num in drafts {
+        for matrix in &mut matrices {
+            // make this a function
+            let mut i: usize = 0;
+            for row in *matrix {
+                if i == 5 {
+                    break;
+                }
+                for (j, guess) in row.iter().enumerate() {
+                    if j == 5 {
+                        break;
+                    }
+                    if guess == &num {
+                        matrix[i][CHECK] += 1;
+                        matrix[CHECK][j] += 1;
+                        // I could break at this point, but I dont know the
+                        // rules of bingo
+                        if matrix[i][CHECK] == 5 {
+                            let row_sum: i32 = matrix[i].iter().sum();
+                            println!("{}", row_sum * num);
+                            return
+                        }
+                        if matrix[CHECK][j] == 5 {
+                            let mut row_sum = 0;
+                            for k in 0..4 {
+                                row_sum += matrix[k][j];
+                                println!("{}", row_sum);
+                            }
+                            println!("{}", row_sum * num);
+                            return
+                        }
+                    }
+                }
+                i += 1;
+            }
+        }
     }
 }
